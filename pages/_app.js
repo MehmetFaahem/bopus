27866e41e10c2c3c49fbbc8c539361ff17068a68
mehmetfaahem <mehmetfaahem@gmail.com>
2022-12-31 15:39:09 +0600

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { Inter } from '@next/font/google'
+import { Inter } from "@next/font/google";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }) {
-  return(
+  return (
     <main className={`${inter.variable} font-sans`}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
